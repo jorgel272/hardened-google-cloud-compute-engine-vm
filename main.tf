bdd36b908fb3178d61adb832a0a5edde459353c2
Jorge Liauw Calo <50120389+jorgel272@users.noreply.github.com>
2023-05-31 13:04:35 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,177 @@
+#------------------------------------------------------------------------------
+# Bootstrap Compute Engine in Google Cloud
+#------------------------------------------------------------------------------
+
+###############################################################################
+# Enable APIs - Enable required APIs for deployment
+###############################################################################
+
+resource "google_project_service" "compute" {
+  service                    = "compute.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+
+resource "google_project_service" "service_networking" {
+  service                    = "servicenetworking.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+
+###############################################################################
+# Create Service Account for Compute Engine Instance 
+###############################################################################
+
+#Create Service Account. Defined project in resource.
+resource "google_service_account" "awesomefunctionalname-sa-ce-environment" {
+  project      = var.gcp_project_id
+  display_name = "Compute Engine Service Account"
+  account_id   = "awesomefunctionalname-sa-ce-environment"
+}
+
+#Add role IAM ServiceAccountUser to created Service Account.
+resource "google_service_account_iam_member" "service_account_user_awesomefunctionalname-sa-ce-environment" {
+  service_account_id = google_service_account.awesomefunctionalname-sa-ce-environment.name
+  member             = format("serviceAccount:%s", google_service_account.awesomefunctionalname-sa-ce-environment.email)
+  role               = "roles/iam.serviceAccountUser"
+}
+
+###############################################################################
+# Deploy Compute Instance reserve internal and public IPv4 addresses
+###############################################################################
+
+# Static Public IPv4 address
+resource "google_compute_address" "public_static_ip" {
+  name   = "ce-AweSomeName-external-ip2"
+  region = var.gcp_region
+}
+
+# Static Internal IPv4 address for Compute Engine Instance
+resource "google_compute_address" "private_static_ip" {
+  address_type = "INTERNAL"
+  region       = var.gcp_region
+  name         = "ce-AweSomeName-internal-ip2"
+  subnetwork   = var.local_vpc_AweSomeName_subnet_name
+  address      = "10.1.20.21"
+}
+
+resource "google_compute_instance" "AweSomeName" {
+  name                      = var.compute_engine_instance_name
+  zone                      = "europe-west4-b"
+  machine_type              = "e2-highmem-8"
+  tags                      = ["http", "http-server", "https", "ping"]
+  project                   = var.gcp_project_id
+  can_ip_forward            = false
+  allow_stopping_for_update = true
+
+  #Primary local VPC network (NIC0)
+
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    network_ip = google_compute_address.private_static_ip.address
+    subnetwork = var.local_vpc_AweSomeName_subnet_name
+
+    access_config {
+      nat_ip = google_compute_address.public_static_ip.address
+    }
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-11-bullseye-v20230306"
+      type  = "pd-ssd"
+      size  = "30"
+    }
+  }
+
+  shielded_instance_config {
+    enable_secure_boot          = true
+    enable_vtpm                 = true
+    enable_integrity_monitoring = true
+  }
+
+  #Define API scopes for GCE Service Account.
+  service_account {
+    email  = google_service_account.awesomefunctionalname-sa-ce-environment.email
+    scopes = var.sa-ce-scopes
+  }
+
+  metadata = {
+    block-project-ssh-keys = true
+    enable-os-login        = false
+    serial-port-enable     = false
+  }
+}
+
+###############################################################################
+# Create firewall rules to allow incoming traffic to Compute Engine
+###############################################################################
+
+# FW Rule 1 - Allow ingress HTTP
+
+resource "google_compute_firewall" "ingress-http-traffic" {
+  name    = "allow-ingress-http"
+  network = var.local_vpc_AweSomeName_network_name
+
+  direction = "INGRESS"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+  target_tags = ["http"]
+}
+
+# FW Rule 2 - Allow ingress HTTPS
+
+resource "google_compute_firewall" "ingress-https-traffic" {
+  name    = "allow-ingress-https"
+  network = var.local_vpc_AweSomeName_network_name
+
+  direction = "INGRESS"
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+  target_tags = ["https"]
+}
+
+# FW Rule 3 - Allow ping
+
+resource "google_compute_firewall" "ingress-ping-traffic" {
+  name    = "allow-ingress-ping"
+  network = var.local_vpc_AweSomeName_network_name
+
+  direction = "INGRESS"
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+  target_tags = ["ping"]
+}
+
+# FW Rule 4  - Allow IAP SSH traffic
+
+resource "google_compute_firewall" "ingress-AweSomeName-ssh-traffic" {
+  name    = "allow-ingress-AweSomeName-ssh"
+  network = var.local_vpc_AweSomeName_network_name
+
+  direction = "INGRESS"
+  priority  = "65534"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = [
+    "35.235.240.0/20"
+  ]
+  target_tags = ["allow-iap-ssh"]
+}
